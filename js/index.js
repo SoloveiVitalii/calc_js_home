e23ad8ee42fe14acf757c24a8bc3259f276171dd
Vitalii <almamatery500@gmail.com>
2018-09-10 11:50:42 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,55 @@
+function plus () {
+	let num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+
+	result = num1 + num2;
+
+	num2 = document.getElementById('eval').innerHTML = result;
+
+}
+
+
+function minus () {
+	let num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 - num2;
+
+	num2 = document.getElementById('eval').innerHTML = result;
+
+}
+
+function multiplication () {
+	let num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 * num2;
+
+	num2 = document.getElementById('eval').innerHTML = result;
+}
+
+function division() {
+	let num1, num2, result;
+	num1 = document.getElementById('n1').value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById('n2').value;
+	num2 = parseInt(num2);
+
+	result = num1 / num2;
+
+	num2 = document.getElementById('eval').innerHTML = result;
+}
